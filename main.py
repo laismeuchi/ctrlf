a36b6b6d5cfa298808eb47828e07f4e06c05a353
lais <laismeuchi@gmail.com>
2024-03-18 16:09:04 -0400

    ajeitar a interface e adicionar instruções de uso

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
-import config
 import pandas
 
 from pathlib import Path
 
 import tkinter
-from tkinter import filedialog, ttk
+from tkinter import filedialog, INSERT, DISABLED, WORD
 from tkinter.messagebox import showinfo
 
 import spacy
 
+words_file_path = ""
+base_file_path = ""
 
-# configuration = config.get_config()
-# palavras_interesse = [x.lower() for x in configuration["palavras_interesse"]]
+
+def browse_words_file():
+    global words_file_path
+    words_file_path = filedialog.askopenfilename(initialdir="/",
+                                                 title="Selecione o Arquivo de Palavras:",
+                                                 filetypes=(("Excel Files",
+                                                             "*.xlsx*"),
+                                                            ("all files",
+                                                             "*.*")))
+
+    words_file_label['text'] = "Arquivo de palavras: " + words_file_path
+    print(words_file_path)
 
 
-def browseFiles():
-    filename = filedialog.askopenfilename(initialdir="/",
-                                          title="Select a File",
-                                          filetypes=(("Excel Files",
-                                                      "*.xlsx*"),
-                                                     ("all files",
-                                                      "*.*")))
+def browse_base_file():
+    global base_file_path
+    base_file_path = filedialog.askopenfilename(initialdir="/",
+                                                title="Selecione o Arquivo de Base:",
+                                                filetypes=(("Excel Files",
+                                                            "*.xlsx*"),
+                                                           ("all files",
+                                                            "*.*")))
+
+    base_file_label['text'] = "Arquivo base: " + base_file_path
+    print(base_file_path)
+
 
 def get_lemas():
     load_model = spacy.load('pt_core_news_sm', disable=['parser'])
@@ -40,13 +56,13 @@ def get_lemas():
 
 
 def treate_files():
-    file_serach_words = words_search_file_explorer_label.cget("text")
+    file_serach_words = words_file_path
 
     xlsx_search_words = pandas.read_excel(file_serach_words)
 
-    palavras_interesse = [x.lower() for x in xlsx_search_words]
+    xlsx_search_words['Palavras'] = xlsx_search_words['Palavras'].str.lower()
 
-    file = file_explorer_label.cget("text")
+    file = base_file_path
     # print(configuration["bases"][file]["caminho"])
     # print(configuration["bases"][file]["aba"])
 
@@ -55,10 +71,8 @@ def treate_files():
     else:
         skip_rows = skip_rows_entry.get()
 
-    # leitura do arquivo xlsx pulando a primeira linha
-    xlsx_file = pandas.read_excel(file,
-                                  sheet_name_entry.get()
-                                  , skiprows=skip_rows)
+    # leitura do arquivo xlsx pulando as linhas informadas
+    xlsx_file = pandas.read_excel(file, sheet_name_entry.get(), skiprows=skip_rows)
 
     # carrega só as colunas que são texto
     text_columns = xlsx_file.select_dtypes(include=object).columns
@@ -79,7 +93,7 @@ def treate_files():
         # para cada coluna da busca, verifica se tem a palavra de interesse
         for column_name in text_columns:
             value_find = str(search_base.at[index, column_name]).lower()
-            for word in palavras_interesse:
+            for word in xlsx_search_words['Palavras']:
                 if word in value_find:
                     search_base.at[index, 'tem_palavra_interesse'] = True
                     key_value = column_name + ": " + word
@@ -97,56 +111,74 @@ def treate_files():
 
 
 if __name__ == '__main__':
+    filename = None
+
     window = tkinter.Tk()
     window.title("Ctrl+F")
     window.iconbitmap('icone.ico')
-    window.config(padx=100, pady=100)
+    window.config(padx=10, pady=10)
 
     informative_label = tkinter.Label(text="Informe os campos abaixo:", font='Calibri 14 bold')
     informative_label.grid(row=0)
 
-    words_search_file_path_label = tkinter.Label(text="Selecione o Arquivo das palavras:", font='Calibri 14')
-    words_search_file_path_label.grid(row=1, column=0)
+    words_file_label = tkinter.Label(text="Arquivo de palavras selecionado: ", font='Calibri 14')
 
-    words_search_browse_button = tkinter.Button(window,
-                                                text="Selecionar Arquivo",
-                                                font='Calibri 12',
-                                                command=browseFiles)
-    words_search_browse_button.grid(row=1, column=1)
+    words_file_browse_button = tkinter.Button(window,
+                                              text="Selecionar arquivo de palavras",
+                                              font='Calibri 12',
+                                              command=browse_words_file)
 
-    words_search_file_explorer_label = tkinter.Label(text="Arquivo de palavras selecionado:", font='Calibri 14')
-    words_search_file_explorer_label.grid(row=2, column=0)
+    words_file_label.grid(row=1, column=0)
+    words_file_browse_button.grid(row=1, column=1)
 
-
-    file_path_label = tkinter.Label(text="Selecione o Arquivo:", font='Calibri 14')
-    file_path_label.grid(row=2, column=0, ipadx=0)
+    base_file_label = tkinter.Label(text="Arquivo base selecionado: ", font='Calibri 14')
 
     file_browse_button = tkinter.Button(window,
-                                        text="Selecionar Arquivo",
+                                        text="Selecionar arquivo base",
                                         font='Calibri 12',
-                                        command=browseFiles)
+                                        command=browse_base_file)
+
+    base_file_label.grid(row=2, column=0)
     file_browse_button.grid(row=2, column=1)
 
-    file_explorer_label = tkinter.Label(text="Arquivo selecionado:", font='Calibri 14')
-    file_explorer_label.grid(row=3, column=0)
-
-
-
     skip_rows_label = tkinter.Label(
-        text="Linhas para pular: (colocar separado por ',' e se não precisar pular deixar em branco)",
+        text="Linhas para pular:",
         font='Calibri 14')
-    skip_rows_label.grid(row=4, column=0)
+    skip_rows_label.grid(row=5, column=0)
 
     skip_rows_entry = tkinter.Entry(window)
-    skip_rows_entry.grid(row=4, column=1)
+    skip_rows_entry.grid(row=5, column=1)
 
     sheet_name_label = tkinter.Label(text="Nome da aba a ser lida:", font='Calibri 14')
-    sheet_name_label.grid(row=5, column=0)
+    sheet_name_label.grid(row=6, column=0)
 
     sheet_name_entry = tkinter.Entry(window)
-    sheet_name_entry.grid(row=5, column=1)
+    sheet_name_entry.grid(row=6, column=1)
 
-    process_file_button = tkinter.Button(text="Processar Arquivo", font='Calibri 12', command=treate_files)
-    process_file_button.grid(row=6, column=1)
+    process_file_button = tkinter.Button(text="Processar Arquivo", font='Calibri 12', fg='white', bg='green',
+                                         command=treate_files)
+    process_file_button.grid(row=8, column=3)
+
+    text_instructions = tkinter.Text(window)
+    text_instructions.insert(INSERT, "Instruções:\n"
+                                     "1 - Selecione o arquivo com as palavras a serem buscadas, no botão 'Selecionar "
+                                     "arquivo de palavras'\n"
+                                     "O arquivo deve ter uma unica coluna chamada 'Palavras' e de estar no formato "
+                                     "XLSX.\n"
+                                     "2 - Selecione o arquivo a base a ser consultada, no botão 'Selecionar arquivo "
+                                     "base'\n"
+                                     "  O arquivo deve estar no formato XLSX.\n"
+                                     "3 - Indique os númveros das linhas a serem puladas na leitura da base, "
+                                     "separados por virgula\n"
+                                     "Esse campo será utilizadao para arquivos que tem linhas antes da linha de "
+                                     "cabeçalho.\n"
+                                     "Para remove-las, basta indicar os números da linhas, iniciando a contagem em 0.\n"
+                                     "4 - Indique o nome da aba do arquivo base que deve ser lida.\n"
+                                     "5 - Clique no botão processar e aguarde a informação de finalização\n"
+                                     "O arquivo será gerado na mesma localização do arquivo base, com o mesmo nome "
+                                     "adicionado o sufixo '_resultado' ao final"
+                             )
+    text_instructions.config(state=DISABLED, wrap=WORD)
+    text_instructions.grid(row=9, column=0, padx=30, pady=100)
 
     window.mainloop()
